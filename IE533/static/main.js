d51d2cb246d9709ce7869b4daafeae362c484bd3
wenxuan-xia <wenxuan.xia@hotmail.com>
2016-04-02 17:53:42 -0400

    change button-color

--- IE533/static/main.js
+++ IE533/static/main.js
@@ -10,12 +10,16 @@ function start() {
     startTime = new Date().getTime();
     $("#action-button-word").html("STOP");
     $("#action-button").attr("onclick", "end()");
+    $("#action-button").removeClass("start-button");
+    $("#action-button").addClass("stop-button");
 }
 
 function end() {
     endTime = new Date().getTime();
     $("#action-button-word").html("START");
     $("#action-button").attr("onclick", "start()");
+    $("#action-button").removeClass("stop-button");
+    $("#action-button").addClass("start-button");
     var milliSecond = endTime-startTime;
-    alert("Your Time:    " + milliSecond/1000.0 + " Second"); 
+    alert("Your Time:   " + milliSecond/1000.0 + " Second");
 }
